864ed472b258220667c58ec642199e8c02b04984
Luke Edwards <luke.edwards05@gmail.com>
2017-09-12 12:00:24 -0700

    initial module logic

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-//
+const critical = require('critical');
+
+const ignore = ['font-face', /@import/, /url\(/];
+
+class WebpackCritical {
+	constructor(opts) {
+		this.generate = this.generate.bind(this);
+		this.opts = Object.assign({ minify:true, inline:true, ignore }, opts);
+	}
+
+	generate(data, callback) {
+		console.log(data);
+		// hard-set option values
+		this.opts.html = data.html;
+		this.opts.dest = data.outputName;
+		this.opts.css = [`build/ssr-build${ data.assets.css[0] }`];
+
+		critical.generate(this.opts, (err, result) => {
+			data.html = result;
+			callback(null, data);
+		});
+	}
+
+	apply(compiler) {
+		compiler.plugin('compilation', bundle => {
+			bundle.plugin('html-webpack-plugin-after-html-processing', this.generate);
+		});
+	}
+}
+
+module.exports = WebpackCritical;
