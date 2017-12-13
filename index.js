cb4d19971a4cdd9411b776e5b0c20944c12517f5
Luke Edwards <luke.edwards05@gmail.com>
2017-12-13 10:12:50 -0800

    update html-plugin hook callback

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const join = require('path').join;
-const readFile = require('fs').readFile;
-const critical = require('inline-critical');
 const minifier = require('html-minifier').minify;
+const critical = require('inline-critical');
 const filterCSS = require('filter-css');
 
 const context = process.cwd();
@@ -9,23 +7,21 @@ const ignore = ['@font-face', /import/, /url\(/];
 
 class WebpackCritical {
 	constructor(opts) {
-		this.generate = this.generate.bind(this);
 		this.opts = Object.assign({ ignore, context }, opts);
 	}
 
-	generate(data, callback) {
-		const file = join(this.opts.context, this.opts.stylesheet || data.assets.css[0]);
-		readFile(file, 'utf8', (err, buf) => {
-			const css = filterCSS(buf, this.opts.ignore);
-			const result = critical(data.html, css, this.opts);
-			const html = minifier(result.toString(), data.plugin.options.minify);
-			callback(null, { html });
-		});
-	}
-
 	apply(compiler) {
+		const opts = this.opts;
+
 		compiler.plugin('compilation', bundle => {
-			bundle.plugin('html-webpack-plugin-after-html-processing', this.generate);
+			bundle.plugin('html-webpack-plugin-after-html-processing', (data, cb) => {
+				const file = opts.stylesheet || data.assets.css[0].substring(1);
+				const source = bundle.assets[file].source();
+				const css = filterCSS(source, opts.ignore);
+				const result = critical(data.html, css, opts);
+				const html = minifier(result.toString(), data.plugin.options.minify);
+				cb(null, { html });
+			});
 		});
 	}
 }
